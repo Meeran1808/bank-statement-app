6f003e582c154e52a86534f26e64b307ca4442f4
Meeran Moidee <meeranmoidee@Admins-MacBook-Air.local>
2025-03-07 14:16:36 +0400

    fix: add quotes and protocol to supabase URL/key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = djsnqurzdsviuhdjuaew.supabase.co
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqc25xdXJ6ZHN2aXVoZGp1YWV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzMzYxMTIsImV4cCI6MjA1NjkxMjExMn0.Q2curmqJh9QJA6fDLYYFBKANKpiCjWrWyuy7zMBHHmc
+const supabaseUrl = 'https://djsnqurzdsviuhdjuaew.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqc25xdXJ6ZHN2aXVoZGp1YWV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzMzYxMTIsImV4cCI6MjA1NjkxMjExMn0.Q2curmqJh9QJA6fDLYYFBKANKpiCjWrWyuy7zMBHHmc'
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
